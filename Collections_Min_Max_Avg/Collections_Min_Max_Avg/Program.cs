973ebd5445c63edab4e3912d6aeb6ad1f262b3d0
ghern <ghern@DESKTOP-OCUVVH0>
2021-10-12 11:40:07 -0500

    Participation COmpleted

---
+++ Collections_Min_Max_Avg/Collections_Min_Max_Avg/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Min_Max_Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examGrades = new List<double>();
+            string answer;
+            do
+            {
+                Console.WriteLine("Please enter an exam grade.");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                examGrades.Add(grade);
+
+                Console.WriteLine("Do you want to enter another exam grade? Yes or No? >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
+
+            double max = 0;
+            double average = 0;
+            double min = 100;
+
+            foreach (double grade in examGrades)
+            {
+                double sum =+ grade;
+                average = sum / examGrades.Count;
+                if (grade <= min)
+                {
+                    min = grade; 
+                }
+                if (grade >= max)
+                {
+                    max = grade;
+                }
+            }
+
+            Console.WriteLine($"Minimum Grade: {min}");
+            Console.WriteLine($"Maximum Grade: {max}");
+            Console.WriteLine($"Average Grade: {average.ToString("N2")}");
+
+        }
+    }
+}
